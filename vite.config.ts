abc04cf6f0d6ea6373b3688854a31f300ad1a772
Giovanni Molina <giovanni.m.05@gmail.com>
2025-04-29 18:30:36 +0100

    feat: Update base URL in Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: '/',
+  base: '/MVPProyectoTitulo/',  // Cambiado a la URL base de GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
